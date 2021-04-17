b70dc44a8219f7bac0a6e7cce4bebe1b78b0ff35
tobi <krisella74@gmail.com>
2021-04-17 10:58:31 +0100

    refactor: move styles to css

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,24 +1,27 @@
 import { useState } from "react";
-import "./App.css"
+import "./App.css";
 
 function App() {
-
   const [show, setShow] = useState(false);
 
-  function dramaticIntro() {
-    !show && setShow(true)
+  function dramaticIntro(): void {
+    !show && setShow(true);
   }
 
   return (
     <div className="App">
-      <h1 style={{
-        transition: 'transform .55s cubic-bezier(.6,-0.1,.2,1.37)',
-        fontSize: 50,
-        marginBottom: 10,
-        transform: show ? 'translateY(0)' : 'translateY(-500px)'
-      }}>Coin-cap</h1>
-      <p style={{ fontStyle: 'italic', opacity: show ? 1 : 0, transition: 'opacity .45s ease 1s'}}>Coming soon</p>
-      <button className="cta" onClick={dramaticIntro}>What's this?</button>
+      <h1
+        className="title"
+        style={{
+          transform: show ? "translateY(0)" : "translateY(-500px)",
+        }}
+      >
+        Coin-cap
+      </h1>
+      <p style={{ opacity: show ? 1 : 0 }}>Coming soon</p>
+      <button className="cta" onClick={dramaticIntro}>
+        What's this?
+      </button>
     </div>
   );
 }
