960a3c2f67e117d98a2f76396dfda9179c9296c5
tobi <krisella74@gmail.com>
2021-04-18 22:27:31 +0100

    consume endpoint and display listings

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,16 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import ListingTable from "./ListingTable"
 import "./App.css";
+
+async function fetchListings(): Promise<any> {
+  const { data } = await fetch(' http://127.0.0.1:5000/listings').then(res => res.json())
+  return data;
+}
+
 
 function App() {
   const [show, setShow] = useState(false);
+  const [listings, setListings] = useState([])
 
   function dramaticIntro(): void {
     !show && setShow(true);
   }
 
+  useEffect(() => {
+    fetchListings().then(data => {
+      setListings(data)
+    })
+  }, [])
+
   return (
     <div className="App">
-      <h1
+      {/* <h1
         className="title"
         style={{
           transform: show ? "translateY(0)" : "translateY(-500px)",
@@ -21,7 +35,15 @@ function App() {
       <p style={{ opacity: show ? 1 : 0 }}>Coming soon</p>
       <button className="cta" onClick={dramaticIntro}>
         What's this?
-      </button>
+      </button> */}
+
+       {/* { listings.map(item => (
+         <Listing />
+       ))} */}
+       { listings.length > 0 ? (
+          <ListingTable listings={listings} />
+       ) : null }
+     
     </div>
   );
 }
---
+++ app/src/ListingTable.tsx
@@ -0,0 +1,50 @@
+
+interface Currency {
+    name: string;
+    symbol: string;
+    quote: {
+      USD: {
+        market_cap: number;
+        price: number
+      }
+    };
+    total_supply: number
+  }
+
+    // format number or price or whatever
+  function formatNumberOrPrice(number: number): string {
+      // because toFixed method returns a string and toLocaleString in our context doesnt work as intended with strings
+      // cast string to number before final chain. 
+      return Number(number.toFixed(2)).toLocaleString()
+  }
+  
+  function ListingTable({ listings }: { listings: Array<Currency>} ) {
+    return (
+      <table>
+        <thead>
+          <tr>
+          <td>No</td>
+        <td>Name</td>
+        <td>Symbol</td>
+        <td>Market cap</td>
+        <td>Price</td>
+        <td>Circulating supply</td>
+          </tr>
+        </thead>
+        <tbody>
+          { listings.map((item, index ) => (
+            <tr>
+             <td>{index+1}</td>
+            <td>{item.name}</td>
+            <td>{item.symbol}</td>
+            <td>{formatNumberOrPrice(item.quote.USD.market_cap)}</td>
+            <td>{formatNumberOrPrice(item.quote.USD.price)}</td>
+            <td>{formatNumberOrPrice(item.total_supply)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    )
+  }
+
+  export default ListingTable
