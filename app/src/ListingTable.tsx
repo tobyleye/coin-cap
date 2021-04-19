9ce6591a203485d5acf26977d4521d6cd7515b8d
tobi <krisella74@gmail.com>
2021-04-19 15:58:41 +0100

    display percentage change

--- app/src/ListingTable.tsx
+++ app/src/ListingTable.tsx
@@ -5,12 +5,17 @@ interface Currency {
     quote: {
       USD: {
         market_cap: number;
-        price: number
+        price: number;
+        percent_change_1h: number;
+        percent_change_24h: number;
       }
     };
     total_supply: number
   }
 
+  function getColor(percent: number): string {
+    return percent >=0 ? 'green' : 'red'
+  }
     // format number or price or whatever
   function formatNumberOrPrice(number: number): string {
       // because toFixed method returns a string and toLocaleString in our context doesnt work as intended with strings
@@ -24,11 +29,13 @@ interface Currency {
         <thead>
           <tr>
           <td>No</td>
-        <td>Name</td>
-        <td>Symbol</td>
-        <td>Market cap</td>
-        <td>Price</td>
-        <td>Circulating supply</td>
+            <td>Name</td>
+            <td>Symbol</td>
+            <td>Market cap</td>
+            <td>Price</td>
+            <td>Circulating supply</td>
+            <td>%1h</td>
+            <td>%24h</td>
           </tr>
         </thead>
         <tbody>
@@ -40,6 +47,12 @@ interface Currency {
             <td>${formatNumberOrPrice(item.quote.USD.market_cap)}</td>
             <td>${formatNumberOrPrice(item.quote.USD.price)}</td>
             <td>${formatNumberOrPrice(item.total_supply)}</td>
+            <td style={{ color: getColor(item.quote.USD.percent_change_1h) }}>
+            {item.quote.USD.percent_change_1h.toFixed(2)}
+            </td>
+            <td style={{ color: getColor(item.quote.USD.percent_change_24h) }}>
+            {item.quote.USD.percent_change_24h.toFixed(2)}
+            </td>
             </tr>
           ))}
         </tbody>
